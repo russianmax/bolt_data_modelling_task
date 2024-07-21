a22bb99f72a5fcd9e45b0257eeb8be8c857aba39
Max Kompanijecs <johndoe504211@gmail.com>
2024-07-21 22:54:01 +0000

    marts

---
+++ models/core/intermediate/int_order_events.sql
@@ -0,0 +1,21 @@
+
+WITH order_events AS (
+    SELECT
+        o.order_id,
+        MIN(CASE WHEN os.order_status = 'Placed' THEN os.order_status_timestamp END) AS order_placed_at,
+        MIN(CASE WHEN os.order_status = 'Ready' THEN os.order_status_timestamp END) AS order_ready_at,
+        MIN(CASE WHEN os.order_status = 'Picked Up' THEN os.order_status_timestamp END) AS order_picked_up_at,
+        MIN(CASE WHEN os.order_status = 'Delivered' THEN os.order_status_timestamp END) AS order_delivered_at
+    FROM {{ ref('fct_orders') }} o
+    JOIN {{ ref('dim_order_status') }} os ON o.order_id = os.order_fk
+    GROUP BY o.order_id
+)
+
+SELECT 
+    order_id,
+    order_placed_at,
+    order_ready_at,
+    order_picked_up_at,
+    order_delivered_at,
+    DATEDIFF(minute, order_placed_at, order_delivered_at) AS delivery_time_minutes,
+FROM order_events
---
+++ models/marts/country_courier_performance_metrics.sql
@@ -0,0 +1,32 @@
+{{ config(
+    materialized='table'
+) }}
+
+WITH country_metrics AS (
+    SELECT
+        co.country_id,
+        co.country_name,
+        COUNT(DISTINCT c.courier_id) AS total_couriers,
+        COUNT(o.order_id) AS total_orders,
+        SUM(o.order_earnings) AS total_earnings,
+        AVG(DATEDIFF(day, oe.order_placed_at, oe.order_ready_at)) AS avg_preparation_time,
+        AVG(DATEDIFF(day, oe.order_ready_at, oe.order_picked_up_at)) AS avg_time_to_pickup,
+        AVG(DATEDIFF(day, oe.order_placed_at, oe.order_delivered_at)) AS avg_delivery_time
+    FROM {{ ref('fct_orders') }} o
+    JOIN {{ ref('dim_courier') }} c ON o.courier_fk = c.courier_id
+    JOIN {{ ref('dim_city') }} ci ON c.current_city_fk = ci.city_id
+    JOIN {{ ref('dim_country') }} co ON ci.country_fk = co.country_id
+    JOIN {{ ref('int_order_events') }} oe ON o.order_id = oe.order_id
+    GROUP BY co.country_id, co.country_name
+)
+
+SELECT
+    country_id,
+    country_name,
+    total_couriers,
+    total_orders,
+    total_earnings,
+    avg_preparation_time,
+    avg_time_to_pickup,
+    avg_delivery_time
+FROM country_metrics
