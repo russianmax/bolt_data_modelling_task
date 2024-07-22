d2f4778dc3a9cfc00c8870cee455441d1249e1d8
Max Kompanijecs <johndoe504211@gmail.com>
2024-07-22 20:24:30 +0000

    incremental models, fct_order_status

--- models/core/fact/fct_courier_order_activity.sql
+++ models/core/fact/fct_courier_order_activity.sql
@@ -1,6 +1,14 @@
+{{ config(
+    materialized='incremental',
+    unique_key='activity_id'
+) }}
+
 WITH source AS (
     SELECT * 
     FROM {{ ref('stg_courier_order_activity') }}
+    {% if is_incremental() %}
+        WHERE activity_timestamp > (SELECT MAX(activity_timestamp) FROM {{ this }})
+    {% endif %}
 )
 
 SELECT
--- models/core/fact/fct_orders.sql
+++ models/core/fact/fct_orders.sql
@@ -1,8 +1,15 @@
+{{ config(
+    materialized='incremental',
+    unique_key='order_id'
+) }}
+
 WITH source AS (
     SELECT * 
     FROM {{ ref('stg_orders') }}
+    {% if is_incremental() %}
+        WHERE order_id > (SELECT MAX(order_id) FROM {{ this }})
+    {% endif %}
 )
-
 SELECT
     order_id,
     courier_fk,
