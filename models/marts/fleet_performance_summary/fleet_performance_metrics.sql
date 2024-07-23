7f63262d4c7443f8f43f3abd171fd097a713b13a
Max Kompanijecs <johndoe504211@gmail.com>
2024-07-23 20:43:02 +0000

    fleet_performance_metrics

--- models/marts/fleet_performance_summary/fleet_performance_metrics.sql
+++ models/marts/fleet_performance_summary/fleet_performance_metrics.sql
@@ -1,18 +1,51 @@
-WITH fleet_performance AS (
-  SELECT
-    f.fleet_name,
-    fm.session_date as date,
-    ci.city_name,
-    co.country_name,
-    f.fleet_type,
-    fm.total_couriers as number_of_couriers_employed,
-    fm.total_working_hours as total_working_hours_provided,
-    fm.total_orders,
-    fm.total_earnings
-FROM {{ ref('int_fleet_metrics') }} fm
-JOIN {{ ref('dim_fleet') }} f ON fm.current_fleet_fk = f.fleet_id
-LEFT JOIN {{ ref('dim_city') }} ci ON f.city_fk = ci.city_id
-LEFT JOIN {{ ref('dim_country') }} co ON ci.country_fk = co.country_id
+WITH country AS (
+    SELECT
+        country_id,
+        country_name
+    FROM {{ ref('dim_country') }}
+),
+
+city AS (
+    SELECT
+        city_id,
+        country_fk,
+        city_name
+    FROM {{ ref('dim_city') }}
+),
+
+fleet AS (
+    SELECT
+        fleet_name,
+        city_fk,
+        fleet_id
+    FROM {{ ref('dim_fleet') }}
+),
+
+fleet_metrics AS (
+    SELECT
+        session_date,
+        total_couriers,
+        total_working_hours,
+        total_orders,
+        total_earnings,
+        current_fleet_fk  -- Ensure this field is included for JOINs
+    FROM {{ ref('int_fleet_metrics') }}
+),
+
+fleet_performance AS (
+    SELECT
+        f.fleet_name,
+        fm.session_date AS date,
+        ci.city_name,
+        co.country_name,
+        fm.total_couriers AS number_of_couriers_employed,
+        fm.total_working_hours AS total_working_hours_provided,
+        fm.total_orders,
+        fm.total_earnings
+    FROM fleet_metrics fm
+    JOIN fleet f ON fm.current_fleet_fk = f.fleet_id
+    LEFT JOIN city ci ON f.city_fk = ci.city_id
+    LEFT JOIN country co ON ci.country_fk = co.country_id
 )
 
 SELECT
@@ -20,11 +53,8 @@ SELECT
     date,
     city_name,
     country_name,
-    fleet_type,
     number_of_couriers_employed,
     total_working_hours_provided,
     total_orders,
     total_earnings
 FROM fleet_performance
-
-
