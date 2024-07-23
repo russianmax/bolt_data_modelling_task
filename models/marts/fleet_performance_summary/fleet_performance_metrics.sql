4e6459432df934f33dbc33e8554f51be4c7cbc9f
Max Kompanijecs <johndoe504211@gmail.com>
2024-07-23 20:50:37 +0000

    missed ,

--- models/marts/fleet_performance_summary/fleet_performance_metrics.sql
+++ models/marts/fleet_performance_summary/fleet_performance_metrics.sql
@@ -17,7 +17,8 @@ fleet AS (
     SELECT
         fleet_name,
         city_fk,
-        fleet_id
+        fleet_id,
+        fleet_type
     FROM {{ ref('dim_fleet') }}
 ),
 
@@ -28,24 +29,25 @@ fleet_metrics AS (
         total_working_hours,
         total_orders,
         total_earnings,
-        current_fleet_fk  -- Ensure this field is included for JOINs
+        current_fleet_fk 
     FROM {{ ref('int_fleet_metrics') }}
 ),
 
 fleet_performance AS (
     SELECT
-        f.fleet_name,
-        fm.session_date AS date,
-        ci.city_name,
-        co.country_name,
-        fm.total_couriers AS number_of_couriers_employed,
-        fm.total_working_hours AS total_working_hours_provided,
-        fm.total_orders,
-        fm.total_earnings
-    FROM fleet_metrics fm
-    JOIN fleet f ON fm.current_fleet_fk = f.fleet_id
-    LEFT JOIN city ci ON f.city_fk = ci.city_id
-    LEFT JOIN country co ON ci.country_fk = co.country_id
+        fleet.fleet_name,
+        fleet.fleet_type,
+        fleet_metrics.session_date AS date,
+        city.city_name,
+        country.country_name,
+        fleet_metrics.total_couriers AS number_of_couriers_employed,
+        fleet_metrics.total_working_hours AS total_working_hours_provided,
+        fleet_metrics.total_orders,
+        fleet_metrics.total_earnings
+    FROM fleet_metrics 
+    JOIN fleet ON fleet_metrics.current_fleet_fk = fleet.fleet_id
+    LEFT JOIN city ON fleet.city_fk = city.city_id
+    LEFT JOIN country ON city.country_fk = country.country_id
 )
 
 SELECT
@@ -53,6 +55,7 @@ SELECT
     date,
     city_name,
     country_name,
+    fleet_type,
     number_of_couriers_employed,
     total_working_hours_provided,
     total_orders,
